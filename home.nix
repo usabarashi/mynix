e1626fc7159bf89a997fab6ee35d95fff6fdf983
usabarashi <19676305+usabarashi@users.noreply.github.com>
2023-05-05 05:13:31 +0900

    Add git.

--- home.nix
+++ home.nix
@@ -6,9 +6,12 @@
   home.stateVersion = "22.11";
 
   home.packages = [
+    pkgs.git
   ];
 
   home.file = {
+    ".config/git/config".source = ./config/git/config;
+    ".ssh/config".source = ./config/ssh/config;
   };
 
   home.sessionVariables = {

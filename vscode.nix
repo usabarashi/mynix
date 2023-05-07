89ae81c21c6f9982b44787a8895a5e4bdeb0566e
usabarashi <19676305+usabarashi@users.noreply.github.com>
2023-05-07 10:56:47 +0900

    Add VSCode.

---
+++ vscode.nix
@@ -0,0 +1,19 @@
+# see: https://github.com/nix-community/home-manager/blob/master/modules/programs/vscode.nix 
+{ config, pkgs, ... }:
+
+{
+  programs.vscode.enable = true;
+
+  programs.vscode.extensions = (with pkgs.vscode-extensions; [
+    jnoortheen.nix-ide
+    ms-azuretools.vscode-docker
+    streetsidesoftware.code-spell-checker
+    vscodevim.vim
+  ]);
+
+  home.activation.vscodeVimConfig = config.lib.dag.entryAfter ["writeBoundary"] ''
+    echo "Setting VSCode Vim Extension configuration..."
+    /usr/bin/defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
+    /usr/bin/defaults write com.microsoft.VSCodeInsiders ApplePressAndHoldEnabled -bool false
+  '';
+}

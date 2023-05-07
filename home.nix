04c610fdeefadcdc9b134debfa91787c8c7f258e
usabarashi <usabarashi@mac.com>
2023-05-07 10:56:47 +0900

    Add VSCode.

--- home.nix
+++ home.nix
@@ -1,14 +1,19 @@
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
 {
   home.username = "gen";
   home.homeDirectory = "/Users/gen";
   home.stateVersion = "22.11";
 
+  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+    "vscode"
+  ];
+
   imports = [
     ./git.nix
     ./karabiner.nix
     ./ssh.nix
+    ./vscode.nix
   ];
 
   home.sessionVariables = {

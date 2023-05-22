67fcdee5f0ba9db2af2e10538684f4b955781d7c
usabarashi <usabarashi@mac.com>
2023-05-23 05:00:10 +0900

    Add GitHub Copilot.

--- vscode.nix
+++ vscode.nix
@@ -1,5 +1,5 @@
 # see: https://github.com/nix-community/home-manager/blob/master/modules/programs/vscode.nix 
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
 {
   programs.vscode.enable = true;
@@ -17,7 +17,16 @@
     ms-azuretools.vscode-docker
     streetsidesoftware.code-spell-checker
     vscodevim.vim
-  ]);
+  ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+    # https://marketplace.visualstudio.com/_apis/public/gallery/publishers/<publisher>/vscode-extensions/<extension-name>/<version>/vspackage
+    {
+      name = "copilot";
+      publisher = "GitHub";
+      version = "1.86.82";
+      sha256 = "sha256-isaqjrAmu/08gnNKQPeMV4Xc8u0Hx8gB2c78WE54kYQ=";
+    }
+  ]
+  );
 
   home.activation.vscodeVimConfig = config.lib.dag.entryAfter ["writeBoundary"] ''
     echo "Setting VSCode Vim Extension configuration..."

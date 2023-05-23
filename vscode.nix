4f7cfcf74ce6f5ba3b99329a63bf46c4d8581bf7
usabarashi <19676305+usabarashi@users.noreply.github.com>
2023-05-23 18:34:30 +0900

    Add vscode-remote-extensionpack,

--- vscode.nix
+++ vscode.nix
@@ -2,26 +2,38 @@
 { config, pkgs, ... }:
 
 {
+  home.packages = with pkgs; [
+    rnix-lsp
+  ];
+
   programs.vscode = {
     enable = true;
-    enableUpdateCheck = true;
-    enableExtensionUpdateCheck = true;
-    mutableExtensionsDir = true;
+    enableUpdateCheck = false;
+    enableExtensionUpdateCheck = false;
+    mutableExtensionsDir = false;
 
     # /Users/USER_NAME/Library/Application Support/Code/User/settings.json
     userSettings = {
-      "editor.tabSize" = 4;
-      "editor.formatOnSave" = true;
+      "editor" = {
+        "formatOnSave" = true;
+        "tabSize" = 4;
+      };
       "[nix]"."editor.tabSize" = 2;
-      "files.insertFinalNewline"= true;
-      "files.trimFinalNewlines"= true;
-      "files.trimTrailingWhitespace" = true;
+
+      "files" = {
+        "insertFinalNewline" = true;
+        "trimFinalNewlines" = true;
+        "trimTrailingWhitespace" = true;
+      };
 
       "extensions.ignoreRecommendations" = false;
+      "nix" = {
+        "enableLanguageServer" = true;
+      };
     };
 
     # /Users/USER_NAME/.vscode/extensions/extensions.json
-    extensions = (with pkgs.vscode-extensions; [
+    extensions = with pkgs.vscode-extensions; [
       jnoortheen.nix-ide
       ms-azuretools.vscode-docker
       streetsidesoftware.code-spell-checker
@@ -34,11 +46,16 @@
         version = "1.86.82";
         sha256 = "sha256-isaqjrAmu/08gnNKQPeMV4Xc8u0Hx8gB2c78WE54kYQ=";
       }
-    ]
-    );
+      {
+        name = "vscode-remote-extensionpack";
+        publisher = "ms-vscode-remote";
+        version = "0.24.0";
+        sha256 = "sha256-6v4JWpyMxqTDIjEOL3w25bdTN+3VPFH7HdaSbgIlCmo";
+      }
+    ];
   };
 
-  home.activation.vscodeVimConfig = config.lib.dag.entryAfter ["writeBoundary"] ''
+  home.activation.vscodeVimConfig = config.lib.dag.entryAfter [ "writeBoundary" ] ''
     echo "Setting VSCode Vim Extension configuration..."
     /usr/bin/defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
     /usr/bin/defaults write com.microsoft.VSCodeInsiders ApplePressAndHoldEnabled -bool false

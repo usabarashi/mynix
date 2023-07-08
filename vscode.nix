dce3f70565ce4764906156e754a971b8fde93214
usabarashi <19676305+usabarashi@users.noreply.github.com>
2023-07-09 07:00:43 +0900

    Add GitHub Pul Requests and Issues

--- vscode.nix
+++ vscode.nix
@@ -19,14 +19,24 @@
         "tabSize" = 4;
       };
 
-      "[json]"."editor.tabSize" = 2;
+      "[json]" = {
+        "editor.tabSize" = 2;
+        "editor.formatOnSave" = true;
+      };
 
-      "[nix]"."editor.tabSize" = 2;
+      "[nix]" = {
+        "editor.tabSize" = 2;
+      };
       "nix"."enableLanguageServer" = true;
 
-      "[shellscript]"."editor.tabSize" = 4;
+      "[shellscript]" = {
+        "editor.tabSize" = 4;
+      };
 
-      "[yaml]"."editor.tabSize" = 2;
+      "[yaml]" = {
+        "editor.tabSize" = 2;
+        "editor.formatOnSave" = true;
+      };
 
       "files" = {
         "insertFinalNewline" = true;
@@ -59,6 +69,12 @@
         publisher = "hediet";
         version = "1.6.6";
         sha256 = "sha256-SPcSnS7LnRL5gdiJIVsFaN7eccrUHSj9uQYIQZllm0M=";
+      }
+      {
+        name = "vscode-pull-request-github";
+        publisher = "GitHub";
+        version = "0.68.0";
+        sha256 = "sha256-mESJ43tsUhYcj3wX8quIZGVo8ecdThueL9rlxRpYCX8=";
       }
       {
         name = "vsliveshare";

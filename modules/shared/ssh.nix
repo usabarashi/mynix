bcb4d4d83d39d4b86d11d266879748b3afc9885a
usabarashi <19676305+usabarashi@users.noreply.github.com>
2024-03-15 15:53:57 +0900

    Add extra SSH configuration file

--- modules/shared/ssh.nix
+++ modules/shared/ssh.nix
@@ -1,9 +1,16 @@
 # see: https://github.com/nix-community/home-manager/blob/master/modules/programs/ssh.nix
 { config, pkgs, ... }:
 
+let
+  extraConfigPath = "~/.ssh/extra_config";
+in
 {
   programs.ssh = {
     enable = true;
+
+    includes = [
+      extraConfigPath
+    ];
 
     matchBlocks = {
       "github.com" = {

75e87acbfa8779354d0791b130fe136ee9e25939
usabarashi <19676305+usabarashi@users.noreply.github.com>
2025-01-16 18:08:44 +0900

    Refactor Docker Compose installation in Nix configuration to use package source and streamline setup

--- modules/shared/container.nix
+++ modules/shared/container.nix
@@ -1,15 +1,6 @@
 { config, pkgs, ... }:
 let
-  os =
-    if pkgs.stdenv.isLinux then "linux"
-    else if pkgs.stdenv.isDarwin then "darwin"
-    else throw "Unsupported architecture";
-  arch =
-    if pkgs.stdenv.isx86_64 then "x86_64"
-    else if pkgs.stdenv.isAarch64 then "aarch64"
-    else throw "Unsupported architecture";
-
-  composeVersion = "v2.23.3";
+  dockerCompose = pkgs.callPackage ../../packages/docker-compose/default.nix { inherit pkgs; };
 in
 {
   home.packages = with pkgs; [
@@ -50,8 +41,7 @@ in
   home.activation.dockerComposeConfig = config.lib.dag.entryAfter [ "writeBoundary" ] ''
     echo "Setting Docker Compose Plugin configuration..."
     mkdir -p ${config.home.homeDirectory}/.docker/cli-plugins
-    /usr/bin/curl -SL https://github.com/docker/compose/releases/download/${composeVersion}/docker-compose-${os}-${arch} \
-      -o ${config.home.homeDirectory}/.docker/cli-plugins/docker-compose
-    chmod a+x ${config.home.homeDirectory}/.docker/cli-plugins/docker-compose
+    rm -f ${config.home.homeDirectory}/.docker/cli-plugins/docker-compose
+    ln -s ${dockerCompose}/cli-plugins/docker-compose ${config.home.homeDirectory}/.docker/cli-plugins/docker-compose
   '';
 }
--- packages/docker-compose/default.nix
+++ packages/docker-compose/default.nix
@@ -1,21 +1,33 @@
-with import <nixpkgs> { };
-stdenv.mkDerivation rec {
+{ pkgs, ... }:
+let
+  os =
+    if pkgs.stdenv.isDarwin then "darwin"
+    else throw "Unsupported architecture";
+  arch =
+    if pkgs.stdenv.isAarch64 then "aarch64"
+    else throw "Unsupported architecture";
+
+  version = "2.23.3";
+  sha256 =
+    if os == "darwin" && arch == "aarch64" then "sha256-chLZPy/92V+JKc+Jq5X6VRiV7xcHcGXKuwD2+FYziIs="
+    else throw "Unsupported combination of OS and architecture";
+
+  src = pkgs.fetchurl {
+    url = "https://github.com/docker/compose/releases/download/v${version}/docker-compose-${os}-${arch}";
+    sha256 = sha256;
+  };
+in
+pkgs.stdenv.mkDerivation {
   pname = "docker-compose";
-  version = "1.0.0";
+  version = version;
 
-  src = ./.;
+  src = src;
 
-  buildInputs = [ ];
+  unpackPhase = "true";
 
   installPhase = ''
     mkdir -p $out/bin
-    cp docker-compose.sh $out/bin/docker-compose
+    cp ${src} $out/bin/docker-compose
     chmod +x $out/bin/docker-compose
   '';
-
-  meta = with lib; {
-    description = "Sample package";
-    homepage = "https://github.com/usabarashi/home-manager";
-    license = licenses.mit;
-  };
 }

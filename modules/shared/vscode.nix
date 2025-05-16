8fc642a6860ec3689b35b4ab324cc6f47a24be6b
usabarashi <19676305+usabarashi@users.noreply.github.com>
2025-05-17 01:55:55 +0900

    Refactor VSCode extensions configuration by removing unused entries

--- modules/shared/vscode.nix
+++ modules/shared/vscode.nix
@@ -48,7 +48,6 @@ in
     extensions = pkgs.vscode-utils.extensionsFromVscodeMarketplace ([
       ###############
       # Base Settings
-
       {
         name = "code-spell-checker";
         publisher = "streetsidesoftware";
@@ -62,64 +61,16 @@ in
         sha256 = "r5xXR4rDbP+2bk66yqPoLod8IZXFrntcKHuWbAiFWwE=";
       }
       {
-        name = "geminicodeassist";
-        publisher = "google";
-        version = "2.32.0";
-        sha256 = "sha256-FRf1DdcDvlxds0RPM8f7kV7cGomJVVM8gx3Mg7O8ALQ=";
-      }
-      {
         name = "gitlens";
         publisher = "eamodio";
         version = "2025.4.905";
         sha256 = "sha256-MieWkv/OCEAvjnpN/AccDEVcAIAWSbl+PBMttvg4E2s=";
       }
       {
-        name = "markdown-mermaid";
-        publisher = "bierner";
-        version = "1.27.0";
-        sha256 = "09w/k1LlGYtyWWbVgoprJG/qB/zCuedF9Cu7kUXcNrE=";
-      }
-      {
-        name = "nix-ide";
-        publisher = "jnoortheen";
-        version = "0.4.16";
-        sha256 = "MdFDOg9uTUzYtRW2Kk4L8V3T/87MRDy1HyXY9ikqDFY=";
-      }
-      {
-        name = "remote-containers";
-        publisher = "ms-vscode-remote";
-        version = "0.408.0";
-        sha256 = "k/c0Ylot3DUJ2UNZDozNmDwuaUAZgPWfuVT16h9eZZI=";
-      }
-      {
-        name = "remote-ssh";
-        publisher = "ms-vscode-remote";
-        version = "0.120.2025040915";
-        sha256 = "sha256-XW7BiUtqFH758I5DDRU2NPdESJC6RfTDAuUA4myY734=";
-      }
-      {
-        name = "run-on-save";
-        publisher = "pucelle";
-        version = "1.10.2";
-        sha256 = "sha256-CRmahf1fbB/5X5f20yUmQXx6D4RghXYDXxfhBPrNUdc=";
-      }
-      {
         name = "vim";
         publisher = "vscodevim";
         version = "1.29.0";
         sha256 = "J3V8SZJZ2LSL8QfdoOtHI1ZDmGDVerTRYP4NZU17SeQ=";
-      }
-      {
-        name = "vscode-docker";
-        publisher = "ms-azuretools";
-        version = "1.29.4";
-        sha256 = "j2ACz2Ww5hddoDLHGdxnuQIqerP5ogZ80/wS+Aa5Gdo=";
-      }
-      {
-        name = "vscode-drawio";
-        publisher = "hediet";
-        version = "1.9.250226013";
-        sha256 = "i4r6tZtOdt1ZzTeITUprtOQl6RncKMhnd4m+BqYqgBk=";
       }
       {
         name = "vscode-github-actions";
@@ -134,39 +85,46 @@ in
         sha256 = "dLg9GAQ9Gu3YZE4PH6UuH0aQitsZJGD4nHv3yqK6nTQ=";
       }
 
-      ################
-      # GitHub Copilot
+      #############
+      # File format
       {
-        name = "copilot";
-        publisher = "github";
-        version = "1.297.0";
-        sha256 = "sha256-UVL0Yf8MSY7ETOxmEK+dljrOQL9ctUWVhbYdr0v00b0=";
+        name = "markdown-mermaid";
+        publisher = "bierner";
+        version = "1.27.0";
+        sha256 = "09w/k1LlGYtyWWbVgoprJG/qB/zCuedF9Cu7kUXcNrE=";
       }
       {
-        # https://docs.github.com/en/copilot/troubleshooting-github-copilot/troubleshooting-issues-with-github-copilot-chat#troubleshooting-issues-caused-by-version-incompatibility
-        # > every new version of Copilot Chat is only compatible with the latest release of Visual Studio Code.
-        # > This means that if you are using an older version of Visual Studio Code, you will not be able to use the latest Copilot Chat.
-        #
-        # update procedures
-        # 1. comment out `github.copilot-chat` and apply.
-        # 2. Check the version of `github.copilot-chat` from VSCode GUI.
-        # 2. Update the version of `github-copilot-chat` and apply.
-        name = "copilot-chat";
-        publisher = "github";
-        version = "0.27.0";
-        sha256 = "sha256-i7KKW+aM8P1nrgnLZssgAlKm1kaQyeh285EnoN9Bwps=";
+        name = "vscode-drawio";
+        publisher = "hediet";
+        version = "1.9.250226013";
+        sha256 = "i4r6tZtOdt1ZzTeITUprtOQl6RncKMhnd4m+BqYqgBk=";
+      }
+
+      ########
+      # Remote
+      {
+        name = "mysql-shell-for-vs-code";
+        publisher = "oracle";
+        version = "1.19.3";
+        sha256 = "t/Z4vOsR2GseFQj5N6VzYiAj0AJiwC1mF2N+LdTwUcM=";
       }
       {
-        name = "vscode-speech";
-        publisher = "ms-vscode";
-        version = "0.12.1";
-        sha256 = "sha256-fxmaPI7uq7DQlzgJc8RcZzHDOwMuodSCf9TkLU9+/+k=";
+        name = "remote-containers";
+        publisher = "ms-vscode-remote";
+        version = "0.408.0";
+        sha256 = "k/c0Ylot3DUJ2UNZDozNmDwuaUAZgPWfuVT16h9eZZI=";
       }
       {
-        name = "vscode-speech-language-pack-ja-jp";
-        publisher = "ms-vscode";
-        version = "0.5.0";
-        sha256 = "sha256-gbesiqyKWPlEPDyAmTgDSbMN9rWRkq1Trsih0gLgPr0=";
+        name = "remote-ssh";
+        publisher = "ms-vscode-remote";
+        version = "0.120.2025040915";
+        sha256 = "sha256-XW7BiUtqFH758I5DDRU2NPdESJC6RfTDAuUA4myY734=";
+      }
+      {
+        name = "vscode-docker";
+        publisher = "ms-azuretools";
+        version = "1.29.4";
+        sha256 = "j2ACz2Ww5hddoDLHGdxnuQIqerP5ogZ80/wS+Aa5Gdo=";
       }
 
       ######################
@@ -211,6 +169,13 @@ in
         version = "2.34.2025012311";
         sha256 = "SmADVhgysDDvmI2/FZHoNWfgSrcxQfJTJj4ZgxOxjhc=";
       }
+      # Nix
+      {
+        name = "nix-ide";
+        publisher = "jnoortheen";
+        version = "0.4.16";
+        sha256 = "MdFDOg9uTUzYtRW2Kk4L8V3T/87MRDy1HyXY9ikqDFY=";
+      }
       # Python
       {
         name = "python";
@@ -232,15 +197,50 @@ in
         sha256 = "zgCqKwnP7Fm655FPUkD5GL+/goaplST8507X890Tnhc=";
       }
 
-      ############
-      # Data Store
+      #####
+      # LLM
 
+      # Gemini
       {
-        name = "mysql-shell-for-vs-code";
-        publisher = "oracle";
-        version = "1.19.3";
-        sha256 = "t/Z4vOsR2GseFQj5N6VzYiAj0AJiwC1mF2N+LdTwUcM=";
+        name = "geminicodeassist";
+        publisher = "google";
+        version = "2.32.0";
+        sha256 = "sha256-FRf1DdcDvlxds0RPM8f7kV7cGomJVVM8gx3Mg7O8ALQ=";
       }
+      # Copilot
+      {
+        name = "copilot";
+        publisher = "github";
+        version = "1.297.0";
+        sha256 = "sha256-UVL0Yf8MSY7ETOxmEK+dljrOQL9ctUWVhbYdr0v00b0=";
+      }
+      {
+        # https://docs.github.com/en/copilot/troubleshooting-github-copilot/troubleshooting-issues-with-github-copilot-chat#troubleshooting-issues-caused-by-version-incompatibility
+        # > every new version of Copilot Chat is only compatible with the latest release of Visual Studio Code.
+        # > This means that if you are using an older version of Visual Studio Code, you will not be able to use the latest Copilot Chat.
+        #
+        # update procedures
+        # 1. comment out `github.copilot-chat` and apply.
+        # 2. Check the version of `github.copilot-chat` from VSCode GUI.
+        # 2. Update the version of `github-copilot-chat` and apply.
+        name = "copilot-chat";
+        publisher = "github";
+        version = "0.27.0";
+        sha256 = "sha256-i7KKW+aM8P1nrgnLZssgAlKm1kaQyeh285EnoN9Bwps=";
+      }
+      {
+        name = "vscode-speech";
+        publisher = "ms-vscode";
+        version = "0.12.1";
+        sha256 = "sha256-fxmaPI7uq7DQlzgJc8RcZzHDOwMuodSCf9TkLU9+/+k=";
+      }
+      {
+        name = "vscode-speech-language-pack-ja-jp";
+        publisher = "ms-vscode";
+        version = "0.5.0";
+        sha256 = "sha256-gbesiqyKWPlEPDyAmTgDSbMN9rWRkq1Trsih0gLgPr0=";
+      }
+
     ]);
   };
 

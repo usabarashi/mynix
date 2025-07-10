7e4a17bd430dd9cde27f7ce997361f35af39288d
usabarashi <19676305+usabarashi@users.noreply.github.com>
2025-07-11 03:57:53 +0900

    Update claudeCodeUsageMonitorExtension to version 0.1.4

--- modules/shared/vscode.nix
+++ modules/shared/vscode.nix
@@ -51,7 +51,7 @@ let
 
   claudeCodeUsageMonitorExtension =
     let
-      version = "0.1.3";
+      version = "0.1.4";
     in
     pkgs.vscode-utils.buildVscodeExtension {
       pname = "claude-code-usage-monitor";
@@ -60,7 +60,7 @@ let
         cp "${
           pkgs.fetchurl {
             url = "https://github.com/usabarashi/vscode-extension-claude-code-usage-monitor/releases/download/v${version}/claude-code-usage-monitor-v${version}.vsix";
-            sha256 = "sha256-qvkKtWB0qT1hsgTfKsjxKr3h2xc1ApqeNomLTdeqJ0w=";
+            sha256 = "sha256-4Qayrcfzvgivfp1glkPOw0iUfMLDVXR74vZ5zcn/jes=";
           }
         }" $out
       '';

f1c5ed97b27a4b1aac08418dbc6ccf8bd41c2c2c
usabarashi <19676305+usabarashi@users.noreply.github.com>
2026-01-20 13:08:31 +0900

    Add default settings

--- modules/shared/neovim.nix
+++ modules/shared/neovim.nix
@@ -11,16 +11,118 @@
     defaultEditor = true;
 
     extraConfig = ''
-      noremap <Tab> <c-w><c-w>
+      " Set leader key to Space
+      let mapleader = " "
+
+      " Buffer navigation with Tab
+      nnoremap <Tab> :bnext<CR>
+      nnoremap <S-Tab> :bprevious<CR>
+
+      " Window navigation with Leader + hjkl
+      nnoremap <leader>h <C-w>h
+      nnoremap <leader>j <C-w>j
+      nnoremap <leader>k <C-w>k
+      nnoremap <leader>l <C-w>l
+
       noremap j gj
       noremap k gk
+
+      " Enable 24-bit color first
+      set termguicolors
+
       set cursorline
+      set cursorcolumn
       set confirm
       set number
+      set relativenumber
+      set clipboard+=unnamedplus
+      set wildmenu
+      set wildmode=longest:full,full
+      set undofile
+      set nowrap
+      set cmdheight=0
+      set noshowmode
+
+      " Search settings
+      set ignorecase
+      set smartcase
+      set hlsearch
+      set incsearch
+
+      " Indent settings
+      set expandtab
+      set tabstop=2
+      set shiftwidth=2
+      set smartindent
+
+      " Scroll offset
+      set scrolloff=8
+
+      " Show buffer list in tabline
+      set showtabline=2
+
+      " Custom tabline to show buffers
+      function! MyTabLine()
+        let s = '''
+        for i in range(1, bufnr('$'))
+          if bufexists(i) && buflisted(i)
+            let s .= (i == bufnr('%') ? '%#TabLineSel#' : '%#TabLine#')
+            let s .= ' ' . i . ':'
+            let name = bufname(i)
+            let name = fnamemodify(name, ':t')
+            if name == '''
+              let name = '[No Name]'
+            endif
+            let s .= name . ' '
+          endif
+        endfor
+        let s .= '%#TabLineFill#'
+        return s
+      endfunction
+      set tabline=%!MyTabLine()
+
+      " netrw (built-in file explorer) settings
+      let g:netrw_liststyle = 3        " tree view
+      let g:netrw_banner = 0           " hide banner
+      let g:netrw_winsize = 25         " window width 25%
+      let g:netrw_browse_split = 0     " open in same window
+      let g:netrw_altv = 1             " split to the right
+
+      " Open netrw with Space + e
+      nnoremap <leader>e :Lexplore<CR>
+
+      " Color scheme
+      set background=dark
+      colorscheme tokyonight-night
+
+      " Status line at bottom (standard placement)
+      function! CurrentMode()
+        let l:mode = mode()
+        if l:mode == 'n'
+          return 'NORMAL'
+        elseif l:mode == 'i'
+          return 'INSERT'
+        elseif l:mode == 'v'
+          return 'VISUAL'
+        elseif l:mode == 'V'
+          return 'V-LINE'
+        elseif l:mode == "\<C-v>"
+          return 'V-BLOCK'
+        elseif l:mode == 'R'
+          return 'REPLACE'
+        elseif l:mode == 't'
+          return 'TERMINAL'
+        else
+          return l:mode
+        endif
+      endfunction
+      set statusline=%{CurrentMode()}\ \|\ %f\ %m%r%h%w\ [%Y]\ [%{&ff}]\ %=%l/%L:%c\ %p%%
+      set laststatus=2
     '';
 
     plugins = with pkgs.vimPlugins; [
       vim-nix
+      tokyonight-nvim
     ];
   };
 

651a8c423534eca627be6be4fd6f4f200b31e65a
usabarashi <19676305+usabarashi@users.noreply.github.com>
2026-02-12 06:15:24 +0900

    Netskope SSL support

--- modules/darwin/netskope-ssl.nix
+++ modules/darwin/netskope-ssl.nix
@@ -17,17 +17,26 @@
   #   sudo launchctl kickstart -k system/org.nixos.nix-daemon
   nix.settings."ssl-cert-file" = "/etc/nix/ca_cert.pem";
 
+  # Expose the combined CA bundle to user-space tools (Rust/rustls, Python requests, curl, etc.)
+  environment.variables = {
+    SSL_CERT_FILE = "/etc/nix/ca_cert.pem";
+    NIX_SSL_CERT_FILE = "/etc/nix/ca_cert.pem";
+    REQUESTS_CA_BUNDLE = "/etc/nix/ca_cert.pem";
+  };
+
   system.activationScripts.postActivation.text = ''
     # Regenerate combined CA certificate bundle (system CAs + Netskope CA)
     NETSKOPE_CERT="/Library/Application Support/Netskope/STAgent/data/nscacert.pem"
     TARGET="/etc/nix/ca_cert.pem"
     if [ -f "$NETSKOPE_CERT" ]; then
+      TMPFILE=$(mktemp)
+      trap 'rm -f "$TMPFILE"' EXIT
       security find-certificate -a -p \
         /System/Library/Keychains/SystemRootCertificates.keychain \
         /Library/Keychains/System.keychain \
-        > /tmp/nix_ca_combined.pem
-      cat /tmp/nix_ca_combined.pem "$NETSKOPE_CERT" > "$TARGET"
-      rm -f /tmp/nix_ca_combined.pem
+        > "$TMPFILE"
+      cat "$TMPFILE" "$NETSKOPE_CERT" > "$TARGET"
+      rm -f "$TMPFILE"
       echo "Netskope SSL: updated $TARGET"
     else
       echo "Netskope SSL: $NETSKOPE_CERT not found, skipping"
